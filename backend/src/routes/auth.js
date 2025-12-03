247d5beeecc2ccdc74b0a33a6f193256974df4c4
treicks <kqg1002@alu.ubu.es>
2025-12-03 21:06:47 +0000

    Se mueven rutas de logout en el backend

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const login = require('../controllers/auth/login.js');
 const check = require('../controllers/auth/check.js');
+const logout = require('../controllers/auth/logout.js');
 const router = express.Router();
 
 router.post('/login', login);
 router.get('/check', check);
+router.get('/logout', logout);
 
 
 module.exports = router;
